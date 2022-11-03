ddc5abebea2731f0e1da564febac9ef1c5c89ccb
max a <maxambrogio1@gmail.com>
2022-11-03 15:00:28 -0400

    additional changes made

--- src/components/ArticleSearchForm.jsx
+++ src/components/ArticleSearchForm.jsx
@@ -1,11 +1,15 @@
 import React, {useState} from "react";
 
 export default function ArticleSearchForm(props){
-    const [query , setQuery] = useState('')
+    const [query , setQuery] = useState('');
+    const [startDate, setStartDate] = useState('');
+    const [endDate, setEndDate] = useState('');
+    const [sort, SetSort] = useState('relevance');
 
     const handleSubmit = (evt) => {
         evt.preventDefault()
         props.onSubmit(query)
+
     }
 
     //use state variables
@@ -14,18 +18,18 @@ export default function ArticleSearchForm(props){
         <form onSubmit={handleSubmit}>
             <input className="search-form" type='text' value={query} onChange={(e) => setQuery(e.target.value)} />
             <div className="date-search">
-                <input type="date" name="start-date" id="start-date" />
+                <input type="date" name="start-date" id="start-date" value={startDate} onChange={(e) => setStartDate(e.target.value)}/>
                 <p> - </p>
-                <input type="date" name="end-date" id="end-date" />
+                <input type="date" name="end-date" id="end-date" value={endDate}   onChange={(e) => setEndDate(e.target.value)} />
             </div>
             <button className="submit" type="submit">Search</button>
             <div className="dropdown">
                 <button className="dropbtn">Filter</button>
-                <div className="dropdown-content">
-                    <a href="#">Newest</a>
-                    <a href="#">Oldest</a>
-                    <a href="#">Relevance</a>
-                </div>
+                <select className="dropdown-content" value={sort} onChange={(e) => SetSort(e.target.value)}>
+                    <option value='newest'>Newest</option>
+                    <option value='oldest'>Oldest</option>
+                    <option value='best_match'>Relevance</option>
+                </select>
             </div>
         </form>
     )
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -9,36 +9,46 @@ export default function Search(props){
     const [searchParams, setSearchParams] = useSearchParams();
     console.log('Search for', searchParams)
     const [articles, setArticles] = useState([])
+    const [startDate, setStartDate] = useState('');
+    const [endDate, setEndDate] = useState('');
+    const [sort, SetSort] = useState('relevance');
 
-
-    // const date = new Date(`${article.pub_date}`)
-    // date.toDateString();
-    //take string as is
-    const beginDate = (date) => (article) => {
-        article.date.toDateString().includes((date || '').toDateString());
-    }
-
-    console.log(beginDate())
+    // if(beginDate)
    
     
-    const q = searchParams.get('q')
+    const q = searchParams.get('q');
     
     
     useEffect(() => {
         if(!q){
             return
-        }
+        };
+        if(!startDate){
+            return
+            // data.start
+        };
+        if(!endDate){
+            return
+            // data.start
+        };
+        if(!sort){
+            return
+            // data.start
+        };
+
         console.log('searching for articles matching: ', q)
         fetch(`${NYT_ARTICLE_ENDPOINT}?q=${q}&api-key=${NYT_API_KEY}`)
         .then((response) => response.json())
         .then((data) => {
             console.log('got data', data)
             setArticles(data.response.docs)
+            
         })
-    }, [q])
+    }, [q, sort, startDate, endDate])
     
-    const handleSearch = (query) => {
+    const handleSearch = (query, sort, startDate, endDate) => {
         setSearchParams( {q: query} )
+
         
         // console.log('query', query)
     }
